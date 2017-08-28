de34fe1f9636f5a1b984a0374053c7a83aca4f43
Shishir Shakya <shishir.shakya@mail.wvu.edu>
2017-08-28 09:30:49 -0400

    script-change with doi

--- Data_scrap.R
+++ Data_scrap.R
@@ -1,3 +1,12 @@
+# # R Script to Download the Historical Data of Aggregated Price and Demand of National Electricity Market of Australia
+# # This R script will download all the historical half-hourly data of aggregated price and demand of all the states of national electricity market of Australia from the Australian Energy Market Operator website and saves into two separate csv file. The data starts from 2:00 AM of 7th of December, 1998 for New South Wales (abbreviated as NSW), Victoria (VIC), Queensland (QLD), South Australia. The data for Tasmania (TAS) starts from 2:00 PM of 15th of May, 2005.
+# 
+# This script has a digital object identifier, please cite as:
+# 
+# Shishir Shakya. (2017, August 27). R Script to Download the Historical Data of Aggregated Price and Demand of National Electricity Market of Australia. Zenodo. http://doi.org/10.5281/zenodo.851555
+# 
+# To export as BibTeX, CSL, DataCite, Dublin Core, JSON, MARCXML and Mendeley, click the link [here](https://zenodo.org/record/851555/export/hx#.WaMC0z6GNaR).
+                                                                                                 
 path <- 'C:/Users/ss0088/data/' #Create the working directory, Make sure the working directory folder has no other files.
 setwd(path)
 
